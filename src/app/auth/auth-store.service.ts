af43d0741c16d9f50def3dd79f2e3b36a0525e61
Prateek Kher <prateek.kher@gmail.com>
2021-05-12 14:41:42 +0530

    AuthStore Implemented

---
+++ src/app/auth/auth-store.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { UserData } from '../shared/collection';
+import { User } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthStoreService {
+  private _loggedIn = false;
+  public user = new BehaviorSubject<User>(this.generateAnonymousUser());
+
+  constructor() { }
+
+  private generateAnonymousUser(): User {
+    return new User(0, 'Anonymous', 0, 0, false, '', '', 0);
+  }
+
+  get loggedIn(): boolean {
+    return this._loggedIn;
+  }
+
+  signIn(userData: UserData): void {
+    const expirationTime = (new Date(userData.updated_at)).getTime() + HOUR;
+    const newUser = new User(
+      userData.id,
+      userData.title,
+      userData.designation_id,
+      userData.department_id,
+      userData.is_admin,
+      userData.mobile,
+      userData.token,
+      expirationTime
+    );
+    this.user.next(newUser);
+    this._loggedIn = true;
+  }
+
+  signOut(): void {
+    this.user.next(this.generateAnonymousUser());
+    this._loggedIn = false;
+  }
+}
