8d6b2b96367bb5e9c83f0ee838a8f70c412153de
Prateek Kher <prateek.kher@gmail.com>
2021-05-12 16:38:41 +0530

    Created SignIn Form

---
+++ src/app/modules/auth/auth.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { SnackBarService } from './../../shared/snack-bar.service';
+import { AuthApiService } from './../../auth/auth-api.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+  signInForm: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: SnackBarService,
+    private auth: AuthApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.signInForm.invalid) {
+      this.snackBar.show('Invalid Data in Form');
+      return;
+    }
+
+    this.auth.signIn(this.username.value, this.password.value)
+    .subscribe(
+      () => {
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        this.snackBar.show(error);
+      }
+    );
+  }
+
+  get username(): FormControl {
+    return this.signInForm.get('username') as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.signInForm.get('password') as FormControl;
+  }
+}
